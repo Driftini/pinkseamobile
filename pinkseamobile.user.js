c2b592c6daa6961bd812356bf703b7383ebbde93
Driftini <yurisilveos741@gmail.com>
2025-03-17 17:48:42 +0100

    Added note about PinkSea's official mobile support in beta

--- pinkseamobile.user.js
+++ pinkseamobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PinkSeaMobile Userscript
 // @namespace    https://github.com/Driftini/pinkseamobile
-// @version      1.0.0
+// @version      1.0.1
 // @description  Complementary userscript for my userstyle to improve drawing on PinkSea from mobile devices
 // @author       Driftini (https://github.com/Driftini)
 // @match        https://pinksea.art/*
@@ -21,6 +21,8 @@ function load(direct = false) {
      *      - /paint was loaded from another page in PinkSea
      */
 
+	const BETA_NOTE = "\n\nNOTE: Mobile support is officially coming to PinkSea!\nIt is recommended to try it on beta.pinksea.art/paint, as it already offers features this userscript does not.\nOnce mobile support comes out of beta on PinkSea, this userscript will be deprecated."
+
     try {
         setTimeout(() => {
             document.getElementById("tegaki-ctrlgrp-layers").setAttribute("tabindex", -1);
@@ -29,14 +31,14 @@ function load(direct = false) {
             let msg = "The PinkSea mobile userscript should have loaded successfully!"
             if (!direct)
                 msg += "\n\nUpon closing this message, you will be prompted to enter a canvas size."
-            alert(msg);
+            alert(msg+BETA_NOTE);
 
             if (!direct)
                 document.querySelector("#tegaki-menu-bar>.tegaki-mb-btn:nth-child(1)").click()
         }, 500);
     }
     catch (e) {
-        alert("The PinkSea mobile userscript has attempted to load, but failed.\n\n" + e);
+        alert("The PinkSea mobile userscript has attempted to load, but failed.\n\n" + e + BETA_NOTE);
     }
 }
 
